b95f12907ad1cb7686f7de2a8f98ff76ef7b1c58
Lance Oreste <lanceoreste@gmail.com>
2024-10-03 16:22:53 -0400

    more code

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Structure for YAML data
+type Config struct {
+	URLs []string `yaml:"urls"`
+}
+
+func main() {
+	config, err := readConfig("urls.yaml")
+	if err != nil {
+		fmt.Println("Error reading YAML:", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	for _, url := range config.URLs {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			checkURL(u)
+		}(url)
+	}
+	wg.Wait()
+}
+
+// Read the URLs from the YAML file
+func readConfig(filename string) (*Config, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+// Function to check URL response and measure time
+func checkURL(url string) {
+	start := time.Now() // Start the timer
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Get(url)
+	elapsed := time.Since(start) // Calculate elapsed time
+
+	if err != nil {
+		fmt.Printf("Failed to reach %s: %v (took %v)\n", url, err, elapsed)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Printf("Received %d from %s (took %v)\n", resp.StatusCode, url, elapsed)
+}
